0f341b54d552dd118db22c480125f2f7277d5bd7
juyeong <yp9782@gmail.com>
2024-11-10 23:54:24 +0900

    promise 로 DB 랩핑

--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
-
-import { createConnection } from 'mysql';
+import mysql from 'mysql2';
 
 dotenv.config();
 
@@ -11,6 +10,43 @@ const app = express();
 const port = process.env.PORT;
 
 app.set(port || 4000);
+
+// DB연결
+const connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : process.env.MYSQL_PW,
+    database : 'todolist',
+});
+
+const connectToDB = () => {
+    return new Promise((resolve, reject) =>{
+        connection.connect ((err) => {
+            if(err){
+                reject('Failed to connect to MySQL: ' + err);
+            } else {
+                resolve('Successfully connected to MySQL');
+            }
+        });
+    });
+};
+
+const fetchData = async() => {
+    try{
+        const message = await connectToDB();
+        console.log(message);
+
+        const [rows, feilds] = await connection.promise().query('SELECT * FROM todolist');
+        console.log(rows);
+
+    } catch(err) {
+        console.error('Error:', err);
+    } finally{
+        connection.end(); //연결종료
+    }
+};
+
+fetchData();
 
 app.get('/', (req,res) => {
     res.send('Welcome to server!');
