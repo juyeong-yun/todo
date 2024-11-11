3e75c304e320cc88deef961c54de5f73420273a0
juyeong <yp9782@gmail.com>
2024-11-11 23:09:37 +0900

    옵셔널체인징 추가

--- react_example/src/components/todoList.js
+++ react_example/src/components/todoList.js
@@ -4,13 +4,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faTrashCan} from '@fortawesome/free-solid-svg-icons';
 
 const TodoList = ({todos, clears, checkedItems, handleCheckboxChange, deleteToDo }) => {
+    
     return(
         <div className='list'>
             <div className='todo'>
                 <h3>List</h3>
                 <ul>
                 {/* todos가 배열임을 보장해야한다. */}
-                {Array.isArray(todos) && todos.length > 0 && (
+                {Array.isArray(todos) && todos?.length > 0 && (
                     todos.map((todo, index) => (
                     <li key={index}>
                         {/* checked : 해당 항목의 체크 상태 */}
