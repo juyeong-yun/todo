b4f92fa1b51630eef5d714068e79635603e6d39b
juyeong <yp9782@gmail.com>
2024-11-11 19:15:52 +0900

    server routes 생성 getList, add

---
+++ server/routes/todoRoutes.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import pool from '../db.js';
+
+const router = express.Router();
+
+// todo 조회
+router.get('/getList', async(req, res) => {
+    try{
+        const [rows] = await pool.query('SELECT * FROM todolist');
+        console.log(rows);
+        res.json(rows);
+    } catch(error) {
+        console.error('Error fetching Data: ', error);
+        res.status(500).json({ message: 'Error fetching data' });
+    }
+});
+
+// todo 추가
+router.post('/add', async(req, res) => {
+    console.log(req.body);
+    const {todo, date} = req.body;
+    
+    // 날짜를 MySQL에 적합한 형식으로 변환
+    const formattedDate = new Date(date).toISOString().split('T')[0]; // YYYY-MM-DD 형식
+
+    try {
+        await pool.query('INSERT INTO todolist (todo, date) VALUES (?, ?)', [todo, formattedDate]);
+        res.json({message : 'Todo added successfully'});
+    } catch (error){
+        console.error('Error adding todo: ', error);
+        res.status(500).json({message: 'Failed to add todo'});
+    }
+});
+
+
+export default router;
