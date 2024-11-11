849d7dc61577ba33e3b59228d7617a5c5aab1db0
juyeong <yp9782@gmail.com>
2024-11-11 20:36:03 +0900

    delete 구현

--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -19,9 +19,9 @@ router.get('/getList', async(req, res) => {
 
 // todo 추가
 router.post('/add', async(req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
     const {todo, date} = req.body;
-    
+
     // 날짜를 MySQL에 적합한 형식으로 변환
     const formattedDate = new Date(date).toISOString().split('T')[0]; // YYYY-MM-DD 형식
 
@@ -32,6 +32,19 @@ router.post('/add', async(req, res) => {
         console.error('Error adding todo: ', error);
         res.status(500).json({message: 'Failed to add todo'});
     }
+});
+
+router.delete('/deleteOne/:id', async(req, res) => {
+    const id = req.params;
+    // console.log(id);
+    
+    try {
+        await pool.query('DELETE FROM todolist WHERE id = ?',[id]);
+        res.json({message : 'Todo deleted successfully'});
+    } catch (error){
+        console.error('Error deleting todo: ', error);
+        res.status(500).json({message: 'Failed to delete todo'});
+    } 
 });
 
 export default router;
