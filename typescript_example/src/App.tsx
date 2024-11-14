7aa9f21a7e201df5d44b73382f6deb10bb91298f
juyeong <yp9782@gmail.com>
2024-11-14 17:17:19 +0900

    addTodo 연결 완료

--- typescript_example/src/App.tsx
+++ typescript_example/src/App.tsx
@@ -1,42 +1,57 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, FormEvent, ChangeEvent} from 'react';
+import { Todo } from './types/Todo';
 import { Advice } from './types/Advice';
-import { Todo } from './types/ToDo';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faPaperPlane, faTrashCan} from '@fortawesome/free-solid-svg-icons';
 
-import checkmark from './public/checkmark.png';
 import './App.css';
 import './reset.css';
 
 function App() {
   const [checkedItems, setCheckedItems] = useState([]);
-  const [todos, setTodos] = useState([]);
-  const [newToDo, setNewTodo] = useState('');
-  const [clears, setClears] = useState([]);
-  const [advice, setAdvice] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newToDo, setNewTodo] = useState<string>('');
+  const [clears, setClears] = useState<Todo[]>([]);
+  const [advice, setAdvice] = useState<string>('');
+
+  function hadleSubmit(e: FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+
+    const addNewToDo : Todo = {
+      todo : newToDo,
+      date : new Date()
+    };
+    
+    setTodos([...todos, addNewToDo]);
+    setNewTodo('');
+  }
+
+  function handleChange(e : ChangeEvent<HTMLInputElement>): void {
+    setNewTodo(e.target.value);
+  }
 
   return (
-    // jsx
       <div className="content">
         <div className='head'>
           <div className='title'>
-            <img src='/checkmark.png' alt='' />
+            <img src='/checkmark.png' alt='icon' />
             <h2>To Do List</h2>
           </div>
           <p>오늘 당신의 해야만 하는 일은 무엇인가요?</p>
         </div>
-        {/*  
+        
         <div className='write'>
-          <form onSubmit = {hadelSubmit} >
-              <input type='text' maxLength='20'
-                  value={newToDo || ''} 
+          <form onSubmit = {hadleSubmit} >
+              <input 
+                  type='text' value={newToDo || ''} 
                   onChange={handleChange} 
-                  placeholder='해야만 하는 일을 적어주세요!' /> 
+                  placeholder='해야만 하는 일을 적어주세요!'
+                  maxLength={20}/> 
               <button type='submit'><FontAwesomeIcon icon={faPaperPlane} /></button>
           </form>
         </div>
+        {/*  
       </div>
-
       <div className='advice'>
         {advice && <span>{advice.message}</span>}
       </div>
@@ -68,8 +83,9 @@ function App() {
             ))}
             </ul>
         </div>
+              */}
       </div>
-      */}
+
   );
 }
 
