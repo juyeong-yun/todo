379ee0382494df1b408de760b6b7e9f01754f899
juyeong <yp9782@gmail.com>
2024-11-14 16:45:36 +0900

    조언타입 정의

--- typescript_example/src/App.tsx
+++ typescript_example/src/App.tsx
@@ -1,69 +1,75 @@
-import React, {useState} from 'react';
-
-import './App.css';
-import './reset.css';
-
+import React, {useState, useEffect} from 'react';
+import { Advice } from './types/Advice';
+import { Todo } from './types/ToDo';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faPaperPlane, faTrashCan} from '@fortawesome/free-solid-svg-icons';
 
-import checkmark from './images/checkmark.png';
+import checkmark from './public/checkmark.png';
+import './App.css';
+import './reset.css';
 
 function App() {
+  const [checkedItems, setCheckedItems] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const [newToDo, setNewTodo] = useState('');
+  const [clears, setClears] = useState([]);
+  const [advice, setAdvice] = useState('');
+
   return (
-    <div className="content">
-      <div className='head'>
-        <div className='title'>
-          <img src={checkmark} alt='' />
-          <h2>To Do List</h2>
+    // jsx
+      <div className="content">
+        <div className='head'>
+          <div className='title'>
+            <img src='/checkmark.png' alt='' />
+            <h2>To Do List</h2>
+          </div>
+          <p>오늘 당신의 해야만 하는 일은 무엇인가요?</p>
         </div>
-        <p>오늘 당신의 해야만 하는 일은 무엇인가요?</p>
+        {/*  
+        <div className='write'>
+          <form onSubmit = {hadelSubmit} >
+              <input type='text' maxLength='20'
+                  value={newToDo || ''} 
+                  onChange={handleChange} 
+                  placeholder='해야만 하는 일을 적어주세요!' /> 
+              <button type='submit'><FontAwesomeIcon icon={faPaperPlane} /></button>
+          </form>
+        </div>
       </div>
 
-      <div className='write'>
-        <form onSubmit = {hadelSubmit} >
-            <input type='text' maxLength='20'
-                value={newToDo || ''} 
-                onChange={handleChange} 
-                placeholder='해야만 하는 일을 적어주세요!' /> 
-            <button type='submit'><FontAwesomeIcon icon={faPaperPlane} /></button>
-        </form>
+      <div className='advice'>
+        {advice && <span>{advice.message}</span>}
       </div>
-    </div>
 
-    <div className='advice'>
-      {advice && <span>{advice.message}</span>}
-    </div>
-
-    <div className='list'>
-      <div className='todo'>
-          <h3>List</h3>
-          <ul>
-          {/* todos가 배열임을 보장해야한다. */}
-          {Array.isArray(todos) && todos?.length > 0 && (
-              todos.map((todo, index) => (
-              <li key={index}>
-                  {/* checked : 해당 항목의 체크 상태 */}
-                  <input type='checkbox' checked={checkedItems[index]}  onClick={() => handleCheckboxChange(todo.id)} />
-                  <span>{todo.todo}</span>
-                  <button type='submit' onClick={() => deleteToDo(todo.id)}>
-                  <FontAwesomeIcon icon={faTrashCan} />
-                  </button>
-              </li>
-              )))}
-          </ul>
+      <div className='list'>
+        <div className='todo'>
+            <h3>List</h3>
+            <ul>
+            {Array.isArray(todos) && todos?.length > 0 && (
+                todos.map((todo, index) => (
+                <li key={index}>
+                    <input type='checkbox' checked={checkedItems[index]}  onClick={() => handleCheckboxChange(todo.id)} />
+                    <span>{todo.todo}</span>
+                    <button type='submit' onClick={() => deleteToDo(todo.id)}>
+                    <FontAwesomeIcon icon={faTrashCan} />
+                    </button>
+                </li>
+                )))}
+            </ul>
+        </div>
+        <div className='clear'>
+            <h3>Clear</h3>
+            <ul>
+            {clears.map((clear, index) => (
+                <li key={index}>
+                    <input type='checkbox' defaultChecked='true' disabled />
+                    <span>{clear.todo}</span>
+                </li>
+            ))}
+            </ul>
+        </div>
       </div>
-      <div className='clear'>
-          <h3>Clear</h3>
-          <ul>
-          {clears.map((clear, index) => (
-              <li key={index}>
-                  <input type='checkbox' defaultChecked='true' disabled />
-                  <span>{clear.todo}</span>
-              </li>
-          ))}
-          </ul>
-      </div>
-    </div>
+      */}
   );
 }
 
