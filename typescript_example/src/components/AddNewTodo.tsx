857001c74f3fed4240d6eb232a8e4d01db015b22
juyeong <yp9782@gmail.com>
2024-11-15 19:51:40 +0900

    todo add 컴포넌트 분할 완료

--- typescript_example/src/components/AddNewTodo.tsx
+++ typescript_example/src/components/AddNewTodo.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faPaperPlane} from '@fortawesome/free-solid-svg-icons';
 
-export const AddNewTodo = () => {
+interface AddTodoProps {
+    newTodo : string;
+    handleChange : (e : React.ChangeEvent<HTMLInputElement>) => void;
+    handleSubmit : (e: React.FormEvent<HTMLFormElement>) => void;
+}
 
+const AddNewTodo : React.FC<AddTodoProps> = ({newTodo, handleChange, handleSubmit}) => {
+    return(
+        <div className='write'>
+            <form onSubmit = {handleSubmit} >
+                <input
+                    type='text' value={newTodo || ''} 
+                    onChange={handleChange} 
+                    placeholder='해야만 하는 일을 적어주세요!'
+                    maxLength={20}/> 
+                <button type='submit'><FontAwesomeIcon icon={faPaperPlane} /></button>
+            </form>
+        </div>
+    );
 };
+
+export default AddNewTodo;
