d5b080090df4825b05a19d7f0fe0ab789568cbe9
juyeong <yp9782@gmail.com>
2024-11-15 19:51:55 +0900

    todo add 컴포넌트 분할 완료

--- typescript_example/src/App.tsx
+++ typescript_example/src/App.tsx
@@ -2,7 +2,8 @@ import React, {useState, useEffect, FormEvent, ChangeEvent} from 'react';
 import { Todo } from './types/Todo';
 import { Advice } from './types/Advice';
 import { getOneAdvice } from './utils/getAdvice';
-
+import AddNewTodo from './components/AddNewTodo';
+import GetTodoList from './components/GetTodoList';
 // import PropsEx from './components/PropsEx';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -14,7 +15,7 @@ import './css/reset.css';
 function App() {
   const [checkedItems, setCheckedItems] = useState<boolean[]>([]);
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [newToDo, setNewTodo] = useState<string>('');
+  const [newTodo, setNewTodo] = useState<string>('');
   const [clears, setClears] = useState<Todo[]>([]);
   const [advice, setAdvice] = useState<Advice|undefined>(undefined);
 
@@ -31,20 +32,20 @@ function App() {
     setNewTodo(e.target.value);
   }
 
-  function hadleSubmit(e: FormEvent<HTMLFormElement>): void {
+  function handleSubmit(e: FormEvent<HTMLFormElement>): void {
     e.preventDefault();
     
-    if(newToDo.trim()){
-      const addNewToDo : Todo = {
-        todo : newToDo, date : new Date()
+    if(newTodo.trim()){
+      const addNewTodo : Todo = {
+        todo : newTodo, date : new Date()
       };
 
-      setTodos([...todos, addNewToDo]);
+      setTodos([...todos, addNewTodo]);
       setNewTodo('');
     }
   }
 
-  function deleteToDo(index:number): void {
+  function deleteTodo(index:number): void {
     setTodos(todos.filter((_, i) => i !== index));
   }
 
@@ -63,59 +64,50 @@ function App() {
   }
 
   return (
-  <div className="content">
-    <div className='head'>
-      <div className='title'>
-        <img src='/checkmark.png' alt='icon' />
-        <h2>To Do List</h2>
-      </div>
-      <p>오늘 당신의 해야만 하는 일은 무엇인가요?</p>
-    </div>
-        
-    <div className='write'>
-      <form onSubmit = {hadleSubmit} >
-          <input 
-              type='text' value={newToDo || ''} 
-              onChange={handleChange} 
-              placeholder='해야만 하는 일을 적어주세요!'
-              maxLength={20}/> 
-          <button type='submit'><FontAwesomeIcon icon={faPaperPlane} /></button>
-      </form>
-    </div>
-      
-    <div className='advice'>
-      {advice && <span>{advice.message}</span>}
-    </div>
-
-    <div className='list'>
-      <div className='todo'>
-          <h3>List</h3>
-          <ul>
-          {Array.isArray(todos) && todos?.length > 0 && (
-              todos.map((todo, index) => (
-              <li key={index}>
-                  <input type='checkbox' checked={checkedItems[index]}  onClick={() => handleCheckboxChange(index)} />
-                  <span>{todo.todo}</span>
-                  <button type='submit' onClick={() => deleteToDo(index)}>
-                  <FontAwesomeIcon icon={faTrashCan} />
-                  </button>
-              </li>
-              )))}
-          </ul>
-          {/* props 이해를 돕기위한 예제 사용  */}
-          {/* <PropsEx todo="Learn TypeScript" isCompleted={false} /> */}
-      </div>
-      <div className='clear'>
-          <h3>Clear</h3>
-          <ul>
-          {clears.map((clear, index) => (
-              <li key={index}>
-                  <input type='checkbox' defaultChecked={true} disabled />
-                  <span>{clear.todo}</span>
-              </li>
-          ))}
-          </ul>
+    <div className="content">
+      <div className='head'>
+        <div className='title'>
+          <img src='/checkmark.png' alt='icon' />
+          <h2>To Do List</h2>
         </div>
+        <p>오늘 당신의 해야만 하는 일은 무엇인가요?</p>
+      </div>
+        
+        <AddNewTodo newTodo={newTodo} handleChange={handleChange} handleSubmit={handleSubmit} />
+        
+        <div className='advice'>
+          {advice && <span>{advice.message}</span>}
+        </div>
+
+        <div className='list'>
+          <div className='todo'>
+              <h3>List</h3>
+              <ul>
+              {Array.isArray(todos) && todos?.length > 0 && (
+                  todos.map((todo, index) => (
+                  <li key={index}>
+                      <input type='checkbox' checked={checkedItems[index]}  onClick={() => handleCheckboxChange(index)} />
+                      <span>{todo.todo}</span>
+                      <button type='submit' onClick={() => deleteTodo(index)}>
+                      <FontAwesomeIcon icon={faTrashCan} />
+                      </button>
+                  </li>
+                  )))}
+              </ul>
+              {/* props 이해를 돕기위한 예제 사용  */}
+              {/* <PropsEx todo="Learn TypeScript" isCompleted={false} /> */}
+          </div>
+          <div className='clear'>
+            <h3>Clear</h3>
+            <ul>
+            {clears.map((clear, index) => (
+                <li key={index}>
+                    <input type='checkbox' defaultChecked={true} disabled />
+                    <span>{clear.todo}</span>
+                </li>
+            ))}
+            </ul>
+          </div>
       </div>
     </div>
   );
